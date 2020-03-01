d2e67eb486caba5534586435b67fd4394dd9ac45
Vlad Fear <onlylastfear1@gmail.com>
2020-03-01 17:57:47 +0200

    WIP
    Signed-off-by: Vlad Fear <onlylastfear1@gmail.com>

--- source/main.cpp
+++ source/main.cpp
@@ -6,32 +6,23 @@
 #include <graphics/shaderprogram.h>
 #include <graphics/vertexshader.h>
 #include <graphics/fragmentshader.h>
+#include <graphics/gamescene.h>
 
 int main(int argc, char** argv)
 {
-	GlobalEngine engine;
-
 	try
 	{
-		engine.initialize();
-	}
-	catch (const std::exception& e)
-	{
-		std::cerr << e.what();
-		return EXIT_FAILURE;
-	}
+		GlobalEngine engine(EngineAPI::OpenGL_API, "GloEngine", 400, 400);
 
-	std::fstream vertex_shader_file("resources/shaders/vertexShader.glsl");
-	VertexShader vshader(vertex_shader_file);
+		std::fstream vertex_shader_file("resources/shaders/vertexShader.glsl");
+		VertexShader vshader(vertex_shader_file);
 
-	std::fstream fragment_shader_file("resources/shaders/fragmentShader.glsl");
-	FragmentShader fshader(fragment_shader_file);
+		std::fstream fragment_shader_file("resources/shaders/fragmentShader.glsl");
+		FragmentShader fshader(fragment_shader_file);
 
-	ShaderProgram triangle1;
-	ShaderProgram triangle2;
+		ShaderProgram triangle1;
+		ShaderProgram triangle2;
 
-	try
-	{
 		vshader.compile();
 		fshader.compile();
 
@@ -42,53 +33,43 @@ int main(int argc, char** argv)
 		triangle2.attachShader(vshader);
 		triangle2.attachShader(fshader);
 		triangle2.compile();
+
+		float vertices[] =
+		{
+			-1.0f,  0.0f, 0.0f, // Left
+			 0.5f,  0.0f, 0.0f, // Right
+			-0.25f, 1.0f, 0.0f  // Top
+		};
+
+		unsigned int indexes[] =
+		{
+			2, 0, 1
+		};
+
+		float vertices1[] =
+		{
+			-0.5f,  0.0f, 0.0f, // Left
+			 1.0f,  0.0f, 0.0f, // Right
+			 0.25f, 1.0f, 0.0f  // Top
+		};
+
+		Model model1(vertices, sizeof(vertices) / sizeof(vertices[0]),
+			indexes, sizeof(indexes) / sizeof(indexes[0]));
+		Model model2(vertices1, sizeof(vertices1) / sizeof(vertices1[0]),
+			indexes, sizeof(indexes) / sizeof(indexes[0]));
+
+		GameScene * gameScene = new GameScene();
+		gameScene->addGameObject(&model1);
+		gameScene->addGameObject(&model2);
+
+		engine.setGameScene(gameScene);
+
+		engine.gameLoop();
 	}
-	catch (const std::exception & e)
+	catch (const std::exception& e)
 	{
 		std::cerr << e.what();
-	}
-
-	GLFWwindow* window = engine.window();
-
-	float vertices[] =
-	{
-		-1.0f,  0.0f, 0.0f, // Left
-		 0.5f,  0.0f, 0.0f, // Right
-		-0.25f, 1.0f, 0.0f  // Top
-	};
-
-	unsigned int indexes[] =
-	{
-		2, 0, 1
-	};
-
-	float vertices1[] =
-	{
-		-0.5f,  0.0f, 0.0f, // Left
-		 1.0f,  0.0f, 0.0f, // Right
-		 0.25f, 1.0f, 0.0f  // Top
-	};
-
-	Model model1(vertices, sizeof(vertices) / sizeof(vertices[0]),
-		indexes, sizeof(indexes) / sizeof(indexes[0]));
-	Model model2(vertices1, sizeof(vertices1) / sizeof(vertices1[0]),
-		indexes, sizeof(indexes) / sizeof(indexes[0]));
-
-	/* Loop until the user closes the window */
-	while (!glfwWindowShouldClose(window))
-	{
-		/* Poll for and process events */
-		engine.pollEvents();
-		engine.clear();
-
-		/* Render here */
-		glUseProgram(triangle1.value());
-		engine.render(model1);
-
-		glUseProgram(triangle2.value());
-		engine.render(model2);
-		/* Swap front and back buffers */
-		engine.swapBuffers();
+		return EXIT_FAILURE;
 	}
 
 	return EXIT_SUCCESS;
