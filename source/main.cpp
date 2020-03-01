eb37276152d920eccbe9bad65bd7e3a472dfcde9
Vlad Fear <onlylastfear1@gmail.com>
2020-03-01 17:57:47 +0200

    WIP
    Signed-off-by: Vlad Fear <onlylastfear1@gmail.com>

--- source/main.cpp
+++ source/main.cpp
@@ -1,94 +1,87 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <graphics/globalengine.h>
-#include <graphics/model.h>
-#include <graphics/shaderprogram.h>
-#include <graphics/vertexshader.h>
-#include <graphics/fragmentshader.h>
+#include <memory>
+
+#include <graphics/GlobalEngine.h>
+#include <graphics/Model.h>
+#include <graphics/ShaderProgram.h>
+#include <graphics/GameScene.h>
 
 int main(int argc, char** argv)
 {
-	GlobalEngine engine;
-
 	try
 	{
-		engine.initialize();
+		std::shared_ptr<GlobalEngine> engine(new GlobalEngine(EngineAPI::OpenGL_API,
+		                                                      "GlobalEngine",
+		                                                      720,
+		                                                      480));
+
+		std::fstream vertex_shader_file("resources/shaders/vertexShader.glsl");
+
+		std::shared_ptr<Shader> vshader = Shader::ShaderBuilder()
+			.setSource(vertex_shader_file)
+			.create(GL_VERTEX_SHADER);
+
+		std::fstream fragment_shader_file("resources/shaders/fragmentShader.glsl");
+
+		std::shared_ptr<Shader> fshader = Shader::ShaderBuilder()
+			.setSource(fragment_shader_file)
+			.create(GL_FRAGMENT_SHADER);
+
+		ShaderProgram triangle1;
+		ShaderProgram triangle2;
+
+		//triangle1.attachShader(vshader);
+		//triangle1.attachShader(fshader);
+		//triangle1.compile();
+
+		//triangle2.attachShader(vshader);
+		//triangle2.attachShader(fshader);
+		//triangle2.compile();
+
+//		float vertices[] =
+//		{
+//			-1.0f,  0.0f, 0.0f, // Left
+//			 0.5f,  0.0f, 0.0f, // Right
+//			-0.25f, 1.0f, 0.0f  // Top
+//		};
+//
+//		unsigned int indexes[] =
+//		{
+//			2, 0, 1
+//		};
+//
+//		float vertices1[] =
+//		{
+//			-0.5f,  0.0f, 0.0f, // Left
+//			 1.0f,  0.0f, 0.0f, // Right
+//			 0.25f, 1.0f, 0.0f  // Top
+//		};
+//
+//		std::shared_ptr<Model> model1 = Model::ModelBuilder()
+//			.addVertexBuffer(vertices, sizeof(float))
+//			.addIndexBuffer(indexes, sizeof(float))
+//			.create();
+//
+//		std::shared_ptr<Model> model2 = Model::ModelBuilder()
+//			.addVertexBuffer(vertices1, sizeof(float))
+//			.addIndexBuffer(indexes, sizeof(float))
+//			.create();
+
+		GameScene * gameScene = new GameScene();
+
+//		gameScene->addGameObject(model1.get());
+//		gameScene->addGameObject(model2.get());
+
+		engine->setGameScene(gameScene);
+
+		engine->gameLoop();
 	}
 	catch (const std::exception& e)
 	{
 		std::cerr << e.what();
 		return EXIT_FAILURE;
-	}
-
-	std::fstream vertex_shader_file("resources/shaders/vertexShader.glsl");
-	VertexShader vshader(vertex_shader_file);
-
-	std::fstream fragment_shader_file("resources/shaders/fragmentShader.glsl");
-	FragmentShader fshader(fragment_shader_file);
-
-	ShaderProgram triangle1;
-	ShaderProgram triangle2;
-
-	try
-	{
-		vshader.compile();
-		fshader.compile();
-
-		triangle1.attachShader(vshader);
-		triangle1.attachShader(fshader);
-		triangle1.compile();
-
-		triangle2.attachShader(vshader);
-		triangle2.attachShader(fshader);
-		triangle2.compile();
-	}
-	catch (const std::exception & e)
-	{
-		std::cerr << e.what();
-	}
-
-	GLFWwindow* window = engine.window();
-
-	float vertices[] =
-	{
-		-1.0f,  0.0f, 0.0f, // Left
-		 0.5f,  0.0f, 0.0f, // Right
-		-0.25f, 1.0f, 0.0f  // Top
-	};
-
-	unsigned int indexes[] =
-	{
-		2, 0, 1
-	};
-
-	float vertices1[] =
-	{
-		-0.5f,  0.0f, 0.0f, // Left
-		 1.0f,  0.0f, 0.0f, // Right
-		 0.25f, 1.0f, 0.0f  // Top
-	};
-
-	Model model1(vertices, sizeof(vertices) / sizeof(vertices[0]),
-		indexes, sizeof(indexes) / sizeof(indexes[0]));
-	Model model2(vertices1, sizeof(vertices1) / sizeof(vertices1[0]),
-		indexes, sizeof(indexes) / sizeof(indexes[0]));
-
-	/* Loop until the user closes the window */
-	while (!glfwWindowShouldClose(window))
-	{
-		/* Poll for and process events */
-		engine.pollEvents();
-		engine.clear();
-
-		/* Render here */
-		glUseProgram(triangle1.value());
-		engine.render(model1);
-
-		glUseProgram(triangle2.value());
-		engine.render(model2);
-		/* Swap front and back buffers */
-		engine.swapBuffers();
 	}
 
 	return EXIT_SUCCESS;
